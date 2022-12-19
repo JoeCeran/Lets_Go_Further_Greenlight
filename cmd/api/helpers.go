a916af4f09c2d00cd95a5fff962e1184710048f3
JoeCeran <joecerankowski@gmail.com>
2022-12-19 12:59:13 -0500

    Completed chapter 2

---
+++ cmd/api/helpers.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"errors"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"strconv"
+)
+
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parameter")
+	}
+
+	return id, nil
+}
